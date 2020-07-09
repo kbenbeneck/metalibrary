be9eeb3ab28c6cd6f13e03e0c5033b064ff3ad20
KBENBENECK <kbenbeneck@gmail.com>
2020-07-09 15:35:26 -0400

    Update album.rb

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,7 +2,9 @@ class Album < ApplicationRecord
   belongs_to :user
   belongs_to :artist
   
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: { scope: :user,
+  message: "already exists" }
+
   
   scope :prog_rock, -> { where(genre: "Progressive Metal") }
   def self.prog_rock
